47865328430aba261177db06d630cd6c08dc5b0b
XO <weedkatz@mail.ru>
2020-09-11 15:35:08 +0900

    second

--- js/script.js
+++ js/script.js
@@ -12,12 +12,30 @@ const personaMoviesDB = {
     privat: false
 };
 
-const a = prompt("Один из последних просмотренных фильмов?", ""),
-      b = +prompt("На сколько вы оцените его?", ""),
-      c = prompt("Один из последних просмотренных фильмов?", ""),
-      d = +prompt("На сколько вы оцените его?", "");
+if (personaMoviesDB.count < 10 && personaMoviesDB.count != null && personaMoviesDB.count != "") {
+    alert('Просмотрено довольно мало фильмов');
+} else if (personaMoviesDB.count >= 10 && personaMoviesDB.count < 30) {
+    alert("Вы классический зритель");
+} else if (personaMoviesDB.count >= 30) {
+    alert("Вы киноман!");
+} else {
+    alert("Произошла ошибка");
+}
 
-personaMoviesDB.movies[a] = b;
-personaMoviesDB.movies[c] = d;
+// for (let i = 0; i < 2; i++) {
+//     const a = prompt("Один из последних просмотренных фильмов?", ""),
+//           b = +prompt("На сколько вы оцените его?", "");
+
+//     if (a != null && b != null && a != "" && b != "" && a.length < 50 ) {
+//         personaMoviesDB.movies[a] = b;
+//         console.log('done');
+//     } else {
+//         console.log('error');
+//         i--;
+//     }
+// }
+
+
+
 
 console.log(personaMoviesDB);
